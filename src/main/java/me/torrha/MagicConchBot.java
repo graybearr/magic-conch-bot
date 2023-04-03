821ecbef5132c4f60d5d60baf5762f8cc47ca75a
torrha <hello@torrha.me>
2023-04-03 22:54:39 +0900

    Initial commit

---
+++ src/main/java/me/torrha/MagicConchBot.java
@@ -0,0 +1,46 @@
+package me.torrha;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import me.torrha.listeners.EventListener;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
+
+import javax.security.auth.login.LoginException;
+
+public class MagicConchBot {
+
+    private final Dotenv config;
+    private final ShardManager shardManager;
+
+    public MagicConchBot() throws LoginException {
+        config = Dotenv.configure().load();
+        String token = config.get("TOKEN");
+
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
+        builder.setStatus(OnlineStatus.ONLINE);
+        builder.setActivity(Activity.listening("당신들의 고민을"));
+        builder.enableIntents(GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT);
+        shardManager = builder.build();
+
+        shardManager.addEventListener(new EventListener());
+    }
+
+    public Dotenv getConfig() {
+        return config;
+    }
+
+    public ShardManager getShardManager() {
+        return shardManager;
+    }
+
+    public static void main(String[] args) {
+        try {
+            MagicConchBot bot = new MagicConchBot();
+        } catch (LoginException e) {
+            System.out.println("ERROR: Provided Bot Token is invalid!");
+        }
+    }
+}
---
+++ src/main/java/me/torrha/listeners/EventListener.java
@@ -0,0 +1,35 @@
+package me.torrha.listeners;
+
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Random;
+
+public class EventListener extends ListenerAdapter {
+    @Override
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+        User user = event.getAuthor();
+        Message message = event.getMessage();
+        if (user.isBot()) return;
+        boolean isMentioned = message.getMentions().getUsers().stream().anyMatch(m -> m.getIdLong() == 1082596268070678648L);
+
+        if (isMentioned) {
+            String content = message.getContentRaw().replaceAll("<@1082596268070678648>", "");
+            String[] answers = {"안돼.", "그래.", "다시 한 번 물어봐.", "둘 다 먹지마.", "굶어.", "그것도 안돼.", "가만히 있어."};
+            Random rand = new Random();
+
+            if (content.length() > 0) {
+                event.getChannel().sendTyping().queue();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                event.getChannel().sendMessage(answers[rand.nextInt(7)]).setMessageReference(message).queue();
+            } else return;
+        }
+    }
+}
